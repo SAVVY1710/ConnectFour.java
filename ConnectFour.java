edb59b51af1514e4c0a7f80e7b31cac6e132a255
SAVVY1710 <163214365+SAVVY1710@users.noreply.github.com>
2024-07-20 03:13:54 -0700

    finished more logic

--- ConnectFour.java
+++ ConnectFour.java
@@ -77,6 +77,44 @@ public class ConnectFour
 
             System.out.print("Player 2, choose a column ");
             ans = scan.nextInt();
+
+            for(int i = 5; i >=0; i --)
+            {
+                if(chars[i][ans-1] == 'O')
+                {
+                    chars[i][ans-1] = 'B';
+                    i =-10;
+                }
+            }
+            for (int i = 0; i < 6; i++)
+            {
+                System.out.println();
+                for (int n = 0; n < 7; n++)
+                {
+                    System.out.print(chars[i][n] + " ");
+                }
+            }
+            System.out.println("\n");
+            if(CheckItA(chars))
+            {
+                System.out.println("Blue Wins");
+                break;
+            }
+            if(CheckItB(chars))
+            {
+                System.out.println("Red Wins");
+                break;
+            }
+            if(diagonalsA(chars))
+            {
+                System.out.println("Blue Wins!");
+                break;
+            }
+            if(diagonalsB(chars))
+            {
+                System.out.println("Red Wins!");
+                break;
+            }
             
         }
     }
@@ -85,7 +123,34 @@ public class ConnectFour
      * @purpose: Method used to check weather red has won
      * @param: Chars is the array used for which charecters are red or blue
      */
-    public boolean CheckItA(char [][] chars){}
+    public boolean CheckItA(char [][] chars)
+    {
+        boolean bool = false;
+        String [] rowcol = new String[13];
+        for (int i = 0; i < 13; i++)rowcol[i] = "";
+        for (int i = 0; i < 6; i++)
+        {
+            for (int n = 0; n < 7; n++)
+            {
+                rowcol[i] += chars[i][n];
+            }
+        }
+        for (int i = 0; i < 7; i++)
+        {
+            for (int n = 0; n < 6; n++)
+            {
+                rowcol[i+6] += chars[n][i];
+            }
+        }
+        for (int i = 0; i < 13; i++)
+        {
+            if(rowcol[i].indexOf("BBBB") >= 0)
+            {
+                bool = true;
+            }
+        }
+        return bool;
+    }
     /*
      * @purpose: Method used to check weather blue has won
      * @param: Chars is the array used for which charecters are red or blue
