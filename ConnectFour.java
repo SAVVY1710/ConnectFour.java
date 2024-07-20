6d9612c78fd25febda2b65c76d9f313cfe47b7ee
SAVVY1710 <163214365+SAVVY1710@users.noreply.github.com>
2024-07-20 01:11:58 -0700

    got some logic

--- ConnectFour.java
+++ ConnectFour.java
@@ -30,6 +30,7 @@ public class ConnectFour
 
         int [] checkarr  = new int[7];
         int highest = 0;
+        //While loop keeps running until someone looses
         while(highest < 4)
         {
             System.out.print("Player 1, choose a column ");
@@ -43,10 +44,43 @@ public class ConnectFour
                     i=-10;
                 }
             }
+            if(CheckItA(chars))
+            {
+                System.out.println("Blue Wins!");
+                break;
+            }
+            if(CheckItB(chars))
+            {
+                System.out.println("Red Wins!");
+                break;
+            }
+            if(diagonalsA(chars))
+            {
+                System.out.println("Blue Wins!");
+                break;
+            }
+            if(diagonalsB(chars))
+            {
+                System.out.println("Red Wins!");
+                break;
+            }
+
+            for (int i = 0; i < 6; i++)
+            {
+                System.out.println();
+                for (int n = 0; n < 7; n++)
+                {
+                    System.out.print(chars[i][n] + " ");
+                }
+            }
+            System.out.println("\n");
+
+            System.out.print("Player 2, choose a column ");
+            ans = scan.nextInt();
             
         }
     }
-    
+
     /*
      * @purpose: Method used to check weather red has won
      * @param: Chars is the array used for which charecters are red or blue
