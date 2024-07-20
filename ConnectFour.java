3e8e894f61f9db2df7cb18bf8296d3b69820ebd2
SAVVY1710 <163214365+SAVVY1710@users.noreply.github.com>
2024-07-20 00:15:30 -0700

    finished base of code

---
+++ ConnectFour.java
@@ -0,0 +1,70 @@
+/*
+Sai Avula
+1.29.24
+ConnectFour
+Make a connect four game
+*/
+import java.util.Scanner;
+public class ConnectFour
+{
+    public static void main(String[]args)
+    {
+        ConnectFour sai = new ConnectFour();
+        sai.runner();
+    }
+    public void runner()
+    {
+        Scanner scan = new Scanner(System.in);
+        char [][] chars = new char[6][7];
+        System.out.println("\n");
+        for (int i = 0; i < 6; i++)
+        {
+            System.out.println();
+            for (int n = 0; n < 7; n++)
+            {
+                chars[i][n] = 'O';
+                System.out.print(chars[i][n] + " ");
+            }
+        }
+        System.out.println("\n");
+
+        int [] checkarr  = new int[7];
+        int highest = 0;
+        while(highest < 4)
+        {
+            System.out.print("Player 1, choose a column ");
+            int ans = scan.nextInt();
+
+            for(int i = 5; i >=0 ; i--)
+            {
+                if (chars[i][ans-1] == 'O')
+                {
+                    chars[i][ans-1] = 'R';
+                    i=-10;
+                }
+            }
+            
+        }
+    }
+    
+    /*
+     * @purpose: Method used to check weather red has won
+     * @param: Chars is the array used for which charecters are red or blue
+     */
+    public boolean CheckItA(char [][] chars){}
+    /*
+     * @purpose: Method used to check weather blue has won
+     * @param: Chars is the array used for which charecters are red or blue
+     */
+    public boolean CheckItB(char [][] chars){}
+    /*
+     * @purpose: Method used to check the diagonals to see wheather red has won
+     * @param: Chars is the array used for which charecters are red or blue
+     */
+    public boolean diagonalsA(char [][] chars){}
+    /*
+     * @purpose:Method used to check the diagonals to see wheather red has won
+     * @param: Chars is the array used for which charecters are red or blue
+     */
+    public boolean diagonalsB(char [][] chars){}
+}
